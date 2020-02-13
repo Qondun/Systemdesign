fa7324a034ee3287c51d370310bb00d175591941
Emanuel Sundin <emanuel.sundin@hotmail.se>
2020-02-13 16:55:31 +0100

    Implemented very basic algorithm, and object to hold the data.

---
+++ public/js/algorithm.js
@@ -0,0 +1,45 @@
+
+
+const Algorithm  = new Vue({
+    data:{
+        men: [],
+        women: [],
+        pairs: []
+    },
+    methods:{
+        stdPair: function(){
+            for(i = 0; i<this.men.length; i++){
+                this.pairs.push(
+                    {
+                        man: this.men[i],
+                        woman: this.women[i]
+                    });
+            }
+        },
+        addPerson: function(person, isMan){
+            if(isMan){
+                this.men.push(person);
+            }
+            else{
+                this.women.push(person);
+            }
+        },
+        printPairs: function(){
+            for (var pair of this.pairs){
+                console.log(pair.man.toString() + " - " + pair.woman.toString());
+            } 
+        },
+        testSetup: function(){
+            this.addPerson("Anders", true); 
+            this.addPerson("Karl", true);
+            this.addPerson("Anna", false);
+            this.addPerson("Elsa", false);
+            
+        }
+    }
+})
+
+
+Algorithm.testSetup();
+Algorithm.stdPair();
+Algorithm.printPairs();

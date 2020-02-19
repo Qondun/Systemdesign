db288134cfcd0e45016e3aea9ed78c099861bd9e
Pontus Melin <pontus.melin.5559@student.uu.se>
2020-02-19 16:57:42 +0100

    added stopwatch.

--- public/js/arranger.js
+++ public/js/arranger.js
@@ -3,6 +3,7 @@
 const vm = new Vue({
     el: '#myID',
     data: {
+      time: '00:00:00.000',
       roundNumber: '1',
       ongoingRound: false,
       eventDone: false
@@ -10,6 +11,7 @@ const vm = new Vue({
     methods: {
         startRound: function (){
             this.ongoingRound = true;
+            //start();
         },
         endRound: function(){
         let rn = this.roundNumber;
@@ -23,3 +25,63 @@ const vm = new Vue({
       }
     }
 })
+
+document.getElementById("startRoundButton").addEventListener("click",start);
+
+var timeBegan = null
+, timeStopped = null
+, stoppedDuration = 0
+, started = null
+, running = false;
+
+function start() {
+  if(running) return;
+  
+  if (timeBegan === null) {
+    reset();
+    timeBegan = new Date();
+  }
+
+  if (timeStopped !== null) {
+    stoppedDuration += (new Date() - timeStopped);
+  }
+
+  started = setInterval(clockRunning, 10);	
+  running = true;
+}
+
+function stop() {
+  running = false;
+  timeStopped = new Date();
+  clearInterval(started);
+}
+
+function reset() {
+  running = false;
+  clearInterval(started);
+  stoppedDuration = 0;
+  timeBegan = null;
+  timeStopped = null;
+  vm.time = "00:00:00.000";
+}
+
+function clockRunning(){
+  var currentTime = new Date()
+  , timeElapsed = new Date(currentTime - timeBegan - stoppedDuration)
+  , hour = timeElapsed.getUTCHours()
+  , min = timeElapsed.getUTCMinutes()
+  , sec = timeElapsed.getUTCSeconds()
+  , ms = timeElapsed.getUTCMilliseconds();
+
+  vm.time = 
+    zeroPrefix(min, 2) + ":" + 
+    zeroPrefix(sec, 2);
+};
+
+function zeroPrefix(num, digit) {
+  var zero = '';
+  for(var i = 0; i < digit; i++) {
+    zero += '0';
+  }
+  return (zero + num).slice(-digit);
+}

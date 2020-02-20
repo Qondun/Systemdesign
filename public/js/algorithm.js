9bae0f7a43a219386a38c7e48fd5554ae5279ebd
Emanuel Sundin <emanuel.sundin@hotmail.se>
2020-02-20 12:52:06 +0100

    Finished making elements clickable. Added next and back buttons.

--- public/js/algorithm.js
+++ public/js/algorithm.js
@@ -1,4 +1,3 @@
-
 const Algorithm  = new Vue({
     el: '#allMatches',
     data:{
@@ -11,17 +10,26 @@ const Algorithm  = new Vue({
             for(i = 0; i<this.men.length; i++){
                 this.pairs.push(
                     {
+                        index: i,
                         man: this.men[i],
-                        woman: this.women[i]
+                        woman: this.women[i],
+                        percentMatch: 99,
+                        selected: false
                     });
             }
         },
         addPerson: function(person, isMan){
             if(isMan){
-                this.men.push(person);
+                this.men.push({
+                   name: person,
+                   picture: "https://reterdeen.com/wp-content/uploads/2019/10/arnold.jpg"
+                });
             }
             else{
-                this.women.push(person);
+                this.women.push({
+                   name: person,
+                   picture: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                });
             }
         },
         printPairs: function(){
@@ -37,11 +45,28 @@ const Algorithm  = new Vue({
             this.addPerson("Karl", true);
             this.addPerson("Anna", false);
             this.addPerson("Elsa", false);
+            this.addPerson("Arnold", true);
+            this.addPerson("Kamilla", false);
             
+        },
+        selectPair: function(index){
+            this.pairs[index].selected = !this.pairs[index].selected;
+            console.log(this.pairs[index].man.name + " - " + this.pairs[index].woman.name);
         }
+        
     }
 })
 
+
+const btn = new Vue({
+    el: '#nextButton',
+    methods: {
+        toRematch: function(){
+            console.log("Till ommatchningen!")
+            //Anything here?
+        }
+    }
+})
 
 Algorithm.testSetup();
 Algorithm.stdPair();

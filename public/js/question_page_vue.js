a7269a79c12fd7db62f4346128e20064f6837476
Ali <ho.ali1010@gmail.com>
2020-02-21 12:39:45 +0100

    added question_page

--- public/js/question_page_vue.js
+++ public/js/question_page_vue.js
@@ -1,35 +1,42 @@
 const vm = new Vue({
-    el: '#myID',
+    el: '#main_box',
     data: {
-        qustionsDone: false,
+        questions: ['lorem ipsum1', 'lorem ipsum2', 'lorem ipsum3'],
+        answers: ['Not answered', 'Not answered', 'Not answered'],
+        questionsDone: false,
         questionNumber: '1',
-        questioning: true
+        triedSubmitting: false
     },
     methods: {
-        startQuestioning: function() {
-            this.questioning = true;
-
-        },
-
-        endQuestion: function() {
-            let qn = this.qustionNumber;
-            if (qn < 3) {
+        incrementNumber: function() {
+            if (this.questionNumber < 4) {
+                let qn = this.questionNumber;
                 qn++;
-            } else {
+                this.questionNumber = qn;
+            }
+        },
+        decrementNumber: function() {
+            if (this.questionNumber > 1) {
+                let qn = this.questionNumber;
+                qn--;
+                this.questionNumber = qn;
+
+            }
+        },
+        addAnswerYes: function() {
+            this.answers[this.questionNumber - 1] = "Yes";
+        },
+        addAnswerNo: function() {
+            this.answers[this.questionNumber - 1] = "No";
+        },
+        submitAnswers: function() {
+            this.triedSubmitting = true;
+            if (!this.answers.includes('Not answered')) {
+                console.log(this.answers);
                 this.questionsDone = true;
             }
-            this.questonNumber = qn;
+
         }
     }
+
 })
-
-document.getElementById("q_yes").addEventListener("click", startQuestioning);
-
-var questioning = false;
-
-function srartQuestioning() {
-    if (questioning) return;
-    if (this.questionNumber < 3) {
-        return this.questionNumber;
-    } else break;
-}

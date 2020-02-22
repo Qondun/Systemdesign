96b8d9bd5d8948bbdbbc536ac8697a7cf73533c8
Emanuel Sundin <emanuel.sundin@hotmail.se>
2020-02-22 15:50:09 +0100

    Rematch functionality.

--- public/js/algorithm.js
+++ public/js/algorithm.js
@@ -9,14 +9,45 @@ const Algorithm  = new Vue({
         pairs: [],
         umatchedMen: [],
         unmatchedWomen: [],
-        newPairs: []
+        newPairs: [],
+        numberOfSelected: 0
+    },
+    created: function () {
+        socket.on('initialize', function (data) {
+            this.pairs = data.orders.pairs;
+            this.setup();
+        }.bind(this));
+        socket.on('currentQueue', function (data) {
+            this.pairs = data.orders.pairs;
+            this.setup();
+        }.bind(this));
     },
     methods:{
+        setup: function(){
+            if(Algorithm.pairs == undefined){
+                Algorithm.pairs = [];
+                Algorithm.testSetup();
+                Algorithm.stdPair();
+            }
+
+
+            //disables or enables button depending on number of selected pairs
+            for (let pair of this.pairs){
+                if(pair.selected){
+                    this.numberOfSelected++;
+                }
+            }
+            let bt = document.getElementById("nextButton");
+            let popText = document.getElementById("popupText");
+            if(this.numberOfSelected<2) {bt.disabled = true; popText.style.visibility = "visible";}
+            else {bt.disabled = false; popText.style.visibility = "hidden";}
+
+
+        },
         stdPair: function(){
             for(var i = 0; i<this.men.length; i++){
                 this.pairs.push(
                     {
-                        index: i,
                         man: this.men[i],
                         woman: this.women[i],
                         percentMatch: 99,
@@ -24,17 +55,17 @@ const Algorithm  = new Vue({
                     });
             }
         },
-        addPerson: function(person, isMan){
+        addPerson: function(person, isMan, pic){
             if(isMan){
                 this.men.push({
                    name: person,
-                   picture: "https://reterdeen.com/wp-content/uploads/2019/10/arnold.jpg"
+                   picture: pic
                 });
             }
             else{
                 this.women.push({
                    name: person,
-                   picture: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                   picture: pic
                 });
             }
         },
@@ -47,19 +78,32 @@ const Algorithm  = new Vue({
             return pair.man.toString() + " - " + pair.woman.toString();
         },
         testSetup: function(){
-            this.addPerson("Anders", true); 
-            this.addPerson("Karl", true);
-            this.addPerson("Anna", false);
-            this.addPerson("Elsa", false);
-            this.addPerson("Arnold", true);
-            this.addPerson("Kamilla", false);
+            this.pairs = []; //Wrong?
+            this.addPerson("Anders", true,
+                "https://imgs.aftonbladet-cdn.se/v2/images/fa271742-05f9-499e-a82f-f42db92048d0?fit=crop&h=1540&q=50&w=1100&s=4f8e7552adaa6475fb10250c173d10c8ff12dd3c"); 
+            this.addPerson("Karl", true,
+                "https://m.media-amazon.com/images/M/MV5BMTg1MjQ0MDg0Nl5BMl5BanBnXkFtZTcwNjYyNjI5Mg@@._V1_UY1200_CR88,0,630,1200_AL_.jpg");
+            this.addPerson("Anna", false,
+                "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+            this.addPerson("Elsa", false,
+                "https://upload.wikimedia.org/wikipedia/commons/b/b2/Natalie_Dormer_2014.jpg");
+            this.addPerson("Arnold", true, 
+                "https://reterdeen.com/wp-content/uploads/2019/10/arnold.jpg");
+            this.addPerson("Kamilla", false,
+                "https://imgs.aftonbladet-cdn.se/v2/images/b27d5d33-e0fd-49d0-b924-2e4c9e697380?fit=crop&h=733&q=50&w=1100&s=8a1306695e56d97efbca205ad72293a21d5c7873");
             
         },
         selectPair: function(index){
             this.pairs[index].selected = !this.pairs[index].selected;
-            console.log(this.pairs[index].man.name + " - " + this.pairs[index].woman.name);
+
+            //disables or enables button depending on number of selected pairs
+            if(this.pairs[index].selected){this.numberOfSelected++;}
+            else {this.numberOfSelected--;}
+            let bt = document.getElementById("nextButton");
+            let popText = document.getElementById("popupText");
+            if(this.numberOfSelected<2) {bt.disabled = true; popText.style.visibility = "visible";}
+            else {bt.disabled = false; popText.style.visibility = "hidden";}
         }
-        
     }
 })
 
@@ -70,10 +114,7 @@ const btn = new Vue({
         toRematch: function(){
             socket.emit('addOrder', 
             {pairs: Algorithm.pairs}
-            )
+            );
         }
     }
 })
-
-Algorithm.testSetup();
-Algorithm.stdPair();
--- public/js/rematching.js
+++ public/js/rematching.js
@@ -9,10 +9,11 @@ const rematching = new Vue({
             name: "",
             picture: "http://guides.global/images/guides/global/dummy_web_page.jpg"
         },
-        unmatchedMen: [],
-        unmatchedWomen: [],
+        sameGender: [],
         differentGender: [],
-        newPairs: []
+        newPairs: [],
+        index: 0,
+        chosenIsMan: true
     },
     created: function () {
         socket.on('initialize', function (data) {
@@ -28,24 +29,71 @@ const rematching = new Vue({
 
         update: function () {
             if (this.pairs != undefined) {
-                this.chosenPerson = this.pairs[0].man;
-                this.unmatchedMen = [];
-                this.unmatchedWomen = [];
+                this.sameGender = [];
+                this.differentGender = [];
+                this.chosenIsMan = true;
                 for (var pair of this.pairs) {
                     if (pair.selected) {
-                        this.unmatchedMen.push(pair.man);
-                        this.unmatchedWomen.push(pair.woman);
+                        this.sameGender.push(pair.man);
+                        this.differentGender.push(pair.woman);
                     }
                     else{
                         this.newPairs.push(pair);
                     }
                 }
-                this.differentGender = this.unmatchedWomen;
-                console.log(this.unmatchedWomen.length)
+                this.chosenPerson = this.sameGender[0];
             }
+            else{
+                this.chosenPerson = {
+                    name: "N/A",
+                    picture: "http://guides.global/images/guides/global/dummy_web_page.jpg"
+                }
+            }
+            this.index = 0;
         },
         changePerson: function (forward) {
+            if(forward) {this.index++;}
+            else {this.index--;}
+            if(this.index < 0){this.index = this.sameGender.length -1}
+            this.index = this.index % this.sameGender.length;
+            this.chosenPerson = (this.sameGender[this.index]);
+        },
+        changeGender: function(){
+            let temp = this.sameGender;
+            this.sameGender = this.differentGender;
+            this.differentGender = temp;
+            this.chosenPerson = this.sameGender[this.index];
+            this.chosenIsMan = !this.chosenIsMan;
+        },
+        reMatch: function(key){
+            if(this.chosenIsMan){
+                this.newPairs.push({
+                    man: this.sameGender[this.index],
+                    woman:  this.differentGender[key],
+                    percentMatch: 95,
+                    selected: false,
+                });
+            }else{
+                this.newPairs.push({
+                    man:  this.differentGender[key],
+                    woman: this.sameGender[this.index],
+                    percentMatch: 95,
+                    selected: false,
+                });
+            }
+            this.sameGender.splice(this.index, 1);
+            this.differentGender.splice(key, 1)
+            
+            if(this.sameGender.length == 0){
 
+                socket.emit('addOrder', 
+                    {pairs: this.newPairs});
+                window.location.href = "/matches"
+            }
+            else if(this.index > this.sameGender.length -1 ) {
+                this.index -= 1;
+            }
+            this.chosenPerson = this.sameGender[this.index];
         }
     }
 })

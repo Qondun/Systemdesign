e67195f1d98cf0fdd58201b731c9eee4a6910f1b
Robin Gurbuz <robin.gurbuz.0439@student.uu.se>
2020-02-24 11:15:35 +0100

    can now print names of matches

--- public/js/matches_vue.js
+++ public/js/matches_vue.js
@@ -1,13 +1,25 @@
 const vm = new Vue({
     el: '#main_box',
     data: {
-        person1: ['John Doe', '40', 'jd@gmail.com', []],
-        person2: ['John Doe2', '40', 'jd2@gmail.com', []],
-        person3: ['John Doe', '40', 'jd@gmail.com', [this.person1, this.person2]],
+        person1: {
+            name: 'John Doe',
+            age: '41',
+            email: 'jd@gmail.com'
+        },
+        person2: {
+            name: 'John Doee',
+            age: '42',
+            email: 'jd2@gmail.com'
+        },
+        person3: {
+            name: 'John Doeee',
+            age: '43',
+            email: 'jd3@gmail.com'
+        },
+        matches: []
     },
-    methods: {
-        test (){
-            console.log(this.person3);
-        }
+    created: function () {
+        this.matches = [this.person1, this.person2, this.person3];
     }
+    
 })

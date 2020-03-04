e10ebc8dc1de33e4992dbe2e2c27bcac4c906a21
anjo4656 <andreas.jonasson.4656@student.uu.se>
2020-03-04 10:10:06 +0100

    User ready up for event no longer sends out round number

--- public/js/user_menu.js
+++ public/js/user_menu.js
@@ -5,7 +5,7 @@ const user_menu = new Vue({
     el: '#main_box',
     methods:{
         start_event: function(){
-            socket.emit('roundToServer', 1);
+            //socket.emit('roundToServer', 1);
             window.location.href = "/show_info";
         }
     }
